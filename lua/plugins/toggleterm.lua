df8d40a33bb6420550cff11a774f632e14343add
Assassin <moinak.dey8@gmail.com>
2023-12-16 21:00:45 +0530

    Java runtime path config for coc.nvim

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return {
             size = 10,
             open_mapping = [[<C-\>]],
             start_in_insert = true,
-            direction = "horizontal",
+            direction = "float",
             float_opts = {
                 border = "curved",
                 width = math.ceil(vim.o.columns*0.8),
