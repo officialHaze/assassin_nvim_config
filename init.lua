97de968c3ea75bd68f38da4cfe646bbbf8a2733c
Assassin <moinak.dey8@gmail.com>
2023-12-18 00:40:19 +0530

    Add kanagawa colorscheme plugin

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- local opts = {} 
-require("vim-options")
+vim.g.mapleader = " "
+
 require("lazy-config")
 require("coc-config")
 require("lazy").setup("plugins", opts)
+require("vim-options")
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,4 +6,4 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd [[let &shell = '"C:/Additional Program Files/Git/bin/bash.exe"']]
 vim.cmd [[let &shellcmdflag = '-s']]
-vim.g.mapleader = " "
+vim.cmd("colorscheme kanagawa")
